8110bc82fd568e5dbb4d30f789ff93b86d8d0eba
kostiantyn_maiuk <maiuk.kostiantyn@gmail.com>
2023-10-02 23:09:02 +0300

    Move shader code into separate files.

--- orion/render_module.cpp
+++ orion/render_module.cpp
@@ -8,18 +8,8 @@
 
 #define log_error	if (SDL_GetError()[0]) {SDL_LogError(0, SDL_GetError()); }
 
-const char *vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-const char *fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\n\0";
+const char *vertexShaderSource;
+const char *fragmentShaderSource;
 
 std::vector<GLfloat> vertices = {
 	0.5f,	0.5f,
