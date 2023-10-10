baefa9a75fdbdb429a75a5141463e06db76cdbdc
kostiantyn_maiuk <maiuk.kostiantyn@gmail.com>
2023-10-10 23:20:57 +0300

    Add class application.

---
+++ orion/application.cpp
@@ -0,0 +1,18 @@
+#include "application.h"
+
+application::application() {}
+
+application::~application() {}
+
+void application::start() {
+	
+}
+
+void application::run() {
+
+}
+
+void application::shut_down() {
+
+}
+
---
+++ orion/application.h
@@ -0,0 +1,15 @@
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+class application {
+public:
+	application();
+	~application();
+
+	void start();
+	void run();
+	void shut_down();
+
+};
+
+#endif // APPLICATION_H
