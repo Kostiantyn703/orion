ce4ced1adc435fbe4f1a7789a9386aae38937b21
kostiantyn_maiuk <maiuk.kostiantyn@gmail.com>
2023-10-16 23:31:25 +0300

    Add game object class.

---
+++ orion/game_object.cpp
@@ -0,0 +1,22 @@
+#include "game_object.h"
+
+#include <SDL_log.h>
+
+game_object::game_object(point &initial_point) {
+	set_position(initial_point.x_pos, initial_point.y_pos);
+}
+
+game_object::game_object(float initial_x, float initial_y) {
+	set_position(initial_x, initial_y);
+}
+
+game_object::~game_object() {}
+
+void game_object::update(float delta_time) {
+	SDL_Log("updating [%f]", delta_time);
+}
+
+void game_object::set_position(float in_x, float in_y) {
+	m_position.x_pos = in_x;
+	m_position.y_pos = in_y; 
+}
---
+++ orion/game_object.h
@@ -0,0 +1,26 @@
+#ifndef GAME_OBJECT_H
+#define GAME_OBJECT_H
+
+struct point {
+	float x_pos;
+	float y_pos;
+};
+
+class game_object {
+public:
+	game_object		(point &initial_point);
+	game_object		(float initial_x, float initial_y);
+	~game_object	();
+
+	void update		(float delta_time);
+
+	void set_position	(float in_x, float in_y);
+	point get_position	() const { return m_position;}
+
+
+private:
+	point m_position;
+
+};
+
+#endif // GAME_OBJECT_H
