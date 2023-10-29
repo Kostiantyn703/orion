950b1005f91b59c9c33523d964534855be9f3767
kostiantyn_maiuk <maiuk.kostiantyn@gmail.com>
2023-10-29 13:45:32 +0200

    Globals: add meteor addresses and names. Content: add meteor images.

--- orion/globals.h
+++ orion/globals.h
@@ -5,15 +5,22 @@ constexpr float		WINDOW_HEIGHT	= 600.f;
 constexpr float		WINDOW_WIDTH	= 800.f;
 static const char	*WINDOW_NAME	= "PROJECT ORION";
 
+// shaders
 static const char	*VERT_SHADER_ADDRESS = "../content/shaders/vertex.vert";
 static const char	*FRAG_SHADER_ADDRESS = "../content/shaders/fragment.frag";
 
-static const char	*SHIP_TEXTURE_ADDRESS = "../content/images/ship.png";
+static const char	*SPRITE_SHADER_NAME = "sprite";
 
-static const char	*SHIP_TEXTURE_NAME	= "ship";
-static const char	*SPRITE_SHADER_NAME	= "sprite";
+// textures
+static const char	*SHIP_TEX_ADDRESS				= "../content/images/ship.png";
+static const char	*METEOR_BROWN_BIG_TEX_ADDRESS	= "../content/images/meteor_brown_big.png";
+static const char	*METEOR_BROWN_MED_TEX_ADDRESS	= "../content/images/meteor_brown_med.png";
 
+static const char	*SHIP_TEX_NAME	= "ship";
+static const char	*METEOR_BROWN_BIG_TEX_NAME	= "meteor_brown_big";
+static const char	*METEOR_BROWN_MED_TEX_NAME	= "meteor_brown_med";
 
+// vertices
 static float vertices[] = {
 	0.f, 1.f,		0.f, 1.f,
 	1.f, 0.f,		1.f, 0.f,
@@ -24,6 +31,7 @@ static float vertices[] = {
 	1.f, 0.f,		1.f, 0.f
 };
 
+// point class
 class point {
 public:
 	point() {}
@@ -35,4 +43,3 @@ public:
 	float y_pos;
 };
 #endif // !GLOBALS_H
-
