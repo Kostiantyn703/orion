7611c8721f3b190a8f40741e41caf8d8299c847c
kostiantyn_maiuk <maiuk.kostiantyn@gmail.com>
2024-01-23 22:35:20 +0200

    Add direction and rotation maps.

--- orion/globals.h
+++ orion/globals.h
@@ -1,8 +1,9 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
+
 // window
-constexpr float		WINDOW_HEIGHT	= 1200.f * 0.8f;	// 600.f
-constexpr float		WINDOW_WIDTH	= 1600.f * 0.8f;	// 800.f
+constexpr float		WINDOW_HEIGHT	= 1200.f * 0.6f;	// 600.f
+constexpr float		WINDOW_WIDTH	= 1600.f * 0.75f;	// 800.f
 static const char	*WINDOW_NAME	= "PROJECT ORION";
 
 constexpr float		VELOCITY_COEFICIENT = 2.f;
@@ -99,4 +100,53 @@ enum class border_side {
 	BS_WEST,
 	BS_NONE = -1
 };
+
+// direction data
+constexpr size_t FV_IDX = 0; // forward vector index
+constexpr size_t ROT_IDX = 1; // rotation index
+
+static const char *DIR_UP_STR = "dir_up";
+static const char *DIR_RIGHT_STR = "dir_right";
+static const char *DIR_DOWN_STR = "dir_down";
+static const char *DIR_LEFT_STR = "dir_left";
+
+#include <map>
+#include <string>
+
+using dir_pair = std::pair<std::string, vector2f>;
+using dir_map = std::map<std::string, vector2f>;
+
+static dir_map g_dir_map;
+
+static void init_dir_map(dir_map &in_map) {
+	dir_pair up		= std::make_pair<std::string, vector2f>(DIR_UP_STR, vector2f(0.f, -1.f));
+	dir_pair right	= std::make_pair<std::string, vector2f>(DIR_RIGHT_STR, vector2f(1.f, 0.f));
+	dir_pair down	= std::make_pair<std::string, vector2f>(DIR_DOWN_STR, vector2f(0.f, 1.f));
+	dir_pair left	= std::make_pair<std::string, vector2f>(DIR_LEFT_STR, vector2f(-1.f, 0.f));
+
+	in_map.insert(up);
+	in_map.insert(right);
+	in_map.insert(down);
+	in_map.insert(left);
+}
+
+using rot_pair = std::pair<std::string, float>;
+using rot_map = std::map<std::string, float>;
+
+static rot_map g_rot_map;
+
+static void init_rot_map(rot_map &in_map) {
+	rot_pair up		= std::make_pair<std::string, float>(DIR_UP_STR, 180.f);
+	rot_pair right	= std::make_pair<std::string, float>(DIR_RIGHT_STR, 90.f);
+	rot_pair down	= std::make_pair<std::string, float>(DIR_RIGHT_STR, 0.f);
+	rot_pair left	= std::make_pair<std::string, float>(DIR_RIGHT_STR, 270);
+
+	in_map.insert(up);
+	in_map.insert(right);
+	in_map.insert(down);
+	in_map.insert(left);
+
+}
+
+
 #endif // !GLOBALS_H
